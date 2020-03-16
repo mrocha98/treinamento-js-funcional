c3621efb1d09fd89c09c9162163b4d8110ad67e4
Matheus Rocha <matheus_rocha98@outlook.com>
2020-03-15 22:50:49 -0300

    finalizando modulo 2

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,9 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import { log } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+import { notasService as service } from './notas/service.js';
 
 document.querySelector('#myButton').onclick = () =>
-  fetch('http://localhost:3000/notas')
-    .then(handleStatus)
-    .then(notas => console.log(notas))
+  service
+    .sumItems('2143')
+    .then(log)
     .catch(console.error);
